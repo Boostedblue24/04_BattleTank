d1c405daacfc287875072ea71695e8906876458e
Evan Oliver <evan.oliver87@gmail.com>
2019-01-22 14:15:58 -0500

    -Correctly added a Tank and TankPlayerController C++ class -Made a secondary Tank_BP to prepare to fix misdirected references

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+
+#include "TankPlayerController.h"
+#include "BattleTank.h"
+#include "Tank.h"
+
+ATank * ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
